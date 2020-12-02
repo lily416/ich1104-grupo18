adde17d76fd8856bb79bca8754c6e76a291b9ed7
Daniela Concha <daconcha@uc.cl>
2020-12-01 23:39:05 -0300

    Paramtros sliders

--- scripts/vortex.js
+++ scripts/vortex.js
@@ -1,56 +1,94 @@
 let 
-  render,           // Canvas
-  div1, detailR1,   // Radio interno del toroide
-  div2, detailR2,   // Radio del tubo del anillo
-  divV, detailV,    // Velocidad de movimiento
-  btnS,             // Botón inicio
-  btnR;             // Botón reinicio
+  render,               // Canvas
+  div1, detailR, valR,  // Radio interno del toroide
+  divk, detailK, valK,  // Circulación del vortice
+  divV, valV,           // Velocidad
+  btnS,                 // Botón inicio
+  btnR;                 // Botón reinicio
 
-let x = 0,          // Posición del toroide en x
-    start = false;  // Simulación iniciada
+let x = 0,              // Posición del toroide en x
+    start = false;      // Simulación iniciada
 
 function setup() {
   render = createCanvas(windowWidth, 300, WEBGL);
   render.parent("vortex");
 
-  div1 = createDiv('Parámetro 1');
+  // ----------------------------------------------------------------
+
+  div1 = createDiv('Radio del vórtice');
   div1.class('p');
   div1.style('color', 'black');
   div1.parent('sliders');
+  
+  valR = createDiv('100');
+  valR.class('p');
+  valR.style('color', 'black');
+  valR.parent('sliders');
+  valR.id('valR');
 
-  detailR1 = createSlider(60, 100, 100);
-  detailR1.parent('sliders');  
+  detailR = createSlider(60, 100, 100);
+  detailR.parent('sliders');  
 
-  div2 = createDiv('Parámetro 2');
-  div2.class('p');
-  div2.style('color', 'black');
-  div2.parent('sliders');  
+  // ----------------------------------------------------------------
 
-  detailR2 = createSlider(10, 30, 30);  
-  detailR2.parent('sliders');  
+  divK = createDiv('Circulación del vórtice');
+  divK.class('p');
+  divK.style('color', 'black');
+  divK.parent('sliders');  
 
-  divV = createDiv('Velocidad');
+  valK = createDiv('5000');
+  valK.class('p');
+  valK.style('color', 'black');
+  valK.parent('sliders');
+  valK.id('valK');
+
+  detailK = createSlider(1000, 9999, 5000);  
+  detailK.parent('sliders');
+
+  // ----------------------------------------------------------------
+
+  divV = createDiv('Velocidad del vórtice');
   divV.class('p');
   divV.style('color', 'black');
-  divV.parent('sliders');  
+  divV.parent('sliders');
 
-  detailV = createSlider(0, 15, 1.5);  
-  detailV.parent('sliders');  
+  valV = createDiv('V: 0');
+  valV.class('p');
+  valV.style('color', 'black');
+  valV.parent('sliders');
+  valV.id('valV')
+
+  // ----------------------------------------------------------------
 
   btnS = createButton('Iniciar simulación');
-  btnS.parent('sliders');
+  btnS.parent('buttons');
   btnS.mousePressed(startSimulation);
   
   btnR = createButton('Reiniciar simulación');
-  btnR.parent('sliders');
+  btnR.parent('buttons');
   btnR.mousePressed(resetSimulation);
 }
 
 function draw() {
   background(205, 102, 94);
+
+  k = detailK.value();
+  R = detailR.value();
+  r = R * .15;
+
+  v = k / (2 * Math.PI * R)
+
+  valR = document.querySelector('#valR');
+  valR.textContent = "R: " + R;
+  
+  valK = document.querySelector('#valK');
+  valK.textContent = "k: " + k;
+  
+  valV = document.querySelector('#valV');
+  valV.textContent = "V: " + v.toFixed(2);
   
   if (start) {
-    x += detailV.value();
+    x += v;
     translate(x, 0, 0);
     rotateY(PI / 2.0);
 
@@ -62,7 +100,7 @@ function draw() {
     rotateY(millis() / 1000);
   }
 
-  torus(detailR1.value(), detailR2.value());
+  torus(R, r);
 }
 
 function windowResized() {
